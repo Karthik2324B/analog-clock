6bd15c69f5d30fbdee8e327247bbd9ebd6989ac9
Karthik2324B <karthik903294@gmail.com>
2024-06-25 20:23:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const hrELe=document.querySelector(".hr");
+const minEle=document.querySelector(".min");
+const secEle=document.querySelector(".sec");
+
+setInterval(()=>
+{
+    let date=new Date();
+
+    hr=date.getHours()*30;
+   
+    min=date.getMinutes()*6;
+    sec=date.getSeconds()*6;
+
+    hrELe.style.transform=`rotateZ(${(hr)+min/12}deg)`;
+    minEle.style.transform=`rotateZ(${(min)}deg)`;
+    secEle.style.transform=`rotateZ(${sec}deg)`;
+
+     
+},1000)
